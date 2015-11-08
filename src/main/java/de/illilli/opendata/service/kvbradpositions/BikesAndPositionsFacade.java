c87fb82fdf9baae4f5c33e293be0ccfd3093cd2e
Wolfram <eberius@gmail.com>
2015-11-08 18:53:25 +0100

    format properties

--- src/main/java/de/illilli/opendata/service/kvbradpositions/BikesAndPositionsFacade.java
+++ src/main/java/de/illilli/opendata/service/kvbradpositions/BikesAndPositionsFacade.java
@@ -2,7 +2,10 @@ package de.illilli.opendata.service.kvbradpositions;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.naming.NamingException;
@@ -25,6 +28,7 @@ public class BikesAndPositionsFacade implements Facade {
 
 	private FeatureCollection featureCollection;
 	private List<BikeBo> bikeList;
+	final static String DATE_FORMAT = "dd.MM.yyyy hh:mm";
 
 	public BikesAndPositionsFacade(SelectBike selectBike) throws SQLException,
 			NamingException, IOException {
@@ -50,11 +54,39 @@ public class BikesAndPositionsFacade implements Facade {
 				Feature feature = new Feature();
 				feature.setId(Integer.toString(key));
 				LineString lineString = new LineString();
+				Map<String, Object> properties = new Hashtable<String, Object>();
+				StringBuilder coordinates = new StringBuilder();
+				StringBuilder timestamps = new StringBuilder();
+				boolean firstRun = true;
 				for (BikeBo bikeBo : bikeList) {
 					LngLatAlt geometry = new LngLatAlt(bikeBo.lng, bikeBo.lat);
 					lineString.add(geometry);
 					feature.setGeometry(lineString);
+					properties.put("name", bikeBo.getName());
+					properties.put("number", bikeBo.getNumber());
+					properties.put("uid", bikeBo.getUid());
+					properties.put("bike", bikeBo.getBike());
+
+					if (firstRun) {
+						firstRun = false;
+					} else {
+						coordinates.append(",");
+						timestamps.append(",");
+					}
+					coordinates.append("[");
+					coordinates.append(bikeBo.lat);
+					coordinates.append(",");
+					coordinates.append(bikeBo.lng);
+					coordinates.append("]");
+
+					String timestamp = new SimpleDateFormat(DATE_FORMAT,
+							Locale.GERMAN).format(bikeBo.getTimestamp());
+					timestamps.append(timestamp);
+
 				}
+				properties.put("coordinates", coordinates.toString());
+				properties.put("timestamps", timestamps);
+				feature.setProperties(properties);
 				featureCollection.add(feature);
 			}
 		}
